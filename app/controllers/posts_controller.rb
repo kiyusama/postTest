00f9f92b45e3af443527739d376b8aea4b8ea67f
kiyusama <kiyusama328@gmail.com>
2023-12-26 17:32:07 +0900

    add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
   end
 
   def create
-  post = Post.new(post_params)
-  post.save
-  redirect_to "/top"
+    post = Post.new(post_params)
+    post.save
+    redirect_to "/top"
   end
 
   def show
@@ -15,6 +15,22 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   private
